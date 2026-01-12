869a17015e4339cd868d8240a6637ba687a35422
Gitae Gu <gitaegu@Gitaeui-MacBookAir.local>
2026-01-13 03:07:41 +0900

    fix: firebase config to use environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,16 +4,16 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC7VDidXaYqcY5RH0UshgBMG_f9LhTAOg0",
-  authDomain: "hyeop-pyeong.firebaseapp.com",
-  projectId: "hyeop-pyeong",
-  storageBucket: "hyeop-pyeong.firebasestorage.app",
-  messagingSenderId: "331149292052",
-  appId: "1:331149292052:web:4ffb775e984fda2b76ad01",
-  measurementId: "G-8WPC71G39X"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measuermentId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// 앱 초기화
+// 앱이 이미 초기화되었는지 확인 (Next.js 리로드 방지)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // ⭐ 여기 앞에 'export'가 붙어 있는지 꼭 확인하세요!
